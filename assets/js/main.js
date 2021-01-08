58d72544f212e826a469780c755f7775d6fdb1e6
Ponchanon Chandra <ponchacc@gmail.com>
2021-01-09 02:21:59 +0600

    Quick Pack Added
    Testing with Quick Start Pack

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".slide").owlCarousel({
+        	items: 4,
+        	loop: true,
+        	dots: true,
+        	nav: false,
+        	margin: 30,
+        	autoplay: false
+        });
+        
+    });
+/*    
+    jQuery(window).load(function(){
+        
+    });*/
+}(jQuery));	
